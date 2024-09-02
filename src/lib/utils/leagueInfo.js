d927aefb4244b0333464147380a44c9bb8d28e54
JDHalfrack <48894405+JDHalfrack@users.noreply.github.com>
2024-09-02 10:01:07 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "917281268096901120"; // your league ID
+export const leagueID = "1050136982275383296"; // your league ID
 export const leagueName = "USCCFFL"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
