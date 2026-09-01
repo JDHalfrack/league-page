9af5c53becff143c5abe56efa006bcd3aa4e8db0
JDHalfrack <48894405+JDHalfrack@users.noreply.github.com>
2026-08-31 22:00:58 -0500

    Enhance Rosters.svelte with teamScores and props
    Added teamScores calculation and updated RosterSorter props.

--- src/lib/Rosters/Rosters.svelte
+++ src/lib/Rosters/Rosters.svelte
@@ -1,28 +1,48 @@
 <script>
-	import { loadPlayers } from '$lib/utils/helper';
-	import RosterSorter from './RosterSorter.svelte'
+    import { loadPlayers } from '$lib/utils/helper';
+    import { calculateTeamWindowScores } from '$lib/utils/helperFunctions/teamWindowScores';
+    import RosterSorter from './RosterSorter.svelte';
 
-	export let leagueData, rosterData, leagueTeamManagers, playersInfo;
+    export let leagueData;
+    export let rosterData;
+    export let leagueTeamManagers;
+    export let playersInfo;
+    export let nflState;
 
-	let players = playersInfo.players;
+    let players = playersInfo.players;
 
-	const refreshPlayers = async () => {
-		const newPlayersInfo = await loadPlayers(null, true);
-		players = newPlayersInfo.players;
-	}
+    $: teamScores = calculateTeamWindowScores({
+        rostersData: rosterData,
+        players,
+        leagueData,
+        currentWeek: nflState?.week || 1
+    });
 
-	if(playersInfo.stale) {
-		refreshPlayers();
-	}
+    const refreshPlayers = async () => {
+        const newPlayersInfo = await loadPlayers(null, true);
+
+        players = newPlayersInfo.players;
+    };
+
+    if (playersInfo.stale) {
+        refreshPlayers();
+    }
 </script>
 
 <style>
-	.rosters {
-		position: relative;
-		z-index: 1;
-	}
+    .rosters {
+        position: relative;
+        z-index: 1;
+    }
 </style>
 
 <div class="rosters">
-	<RosterSorter rosters={rosterData.rosters} {players} {leagueTeamManagers} startersAndReserve={rosterData.startersAndReserve} {leagueData} />
+    <RosterSorter
+        rosters={rosterData.rosters}
+        {players}
+        {leagueTeamManagers}
+        startersAndReserve={rosterData.startersAndReserve}
+        {leagueData}
+        {teamScores}
+    />
 </div>
