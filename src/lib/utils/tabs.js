48f7296fffaa30569445c32943e966d2ab5250d8
JDHalfrack <48894405+JDHalfrack@users.noreply.github.com>
2026-09-03 11:08:02 -0500

    Refactor tab definitions for consistency

--- src/lib/utils/tabs.js
+++ src/lib/utils/tabs.js
@@ -1,35 +1,12 @@
 import {leagueID} from '$lib/utils/leagueInfo';
 
 export const tabs = [
+    { icon: 'home', label: 'Home', dest: '/', key: 'home' },
+    { icon: 'sports', label: 'Matchups', dest: '/matchups', key: 'matchups' },
+    { icon: 'swap_horiz', label: 'Trades & Waivers', dest: '/transactions', key: 'transactions' },
+    { icon: 'article', label: 'Blog', dest: '/blog', key: 'blog' },
     {
-        icon: 'home',
-        label: 'Home',
-        dest: '/',
-        key: 'home',
-    },
-    {
-        icon: 'sports',
-        label: 'Matchups',
-        dest: '/matchups',
-        key: 'matchups',
-    },
-    {
-        icon: 'swap_horiz',
-        label: 'Trades & Waivers',
-        dest: '/transactions',
-        key: 'transactions',
-    },
-    {
-        icon: 'article',
-        label: 'Blog',
-        dest: '/blog',
-        key: 'blog',
-    },
-    {
-        icon: 'view_comfy',
-        label: 'League Info',
-        nest: true,
-        key: 'league_info',
+        icon: 'view_comfy', label: 'League Info', nest: true, key: 'league_info',
         children: [
             { icon: 'storage', label: 'Rosters', dest: '/rosters' },
             { icon: 'groups', label: 'Managers', dest: '/managers' },
@@ -42,30 +19,15 @@ export const tabs = [
             { icon: 'swap_calls', label: 'Trade Analyzer', dest: '/trade-analyzer' },
             { icon: 'hourglass_top', label: 'Keeper Tracker', dest: '/keeper-tracker' },
             { icon: 'history_edu', label: 'Constitution', dest: '/constitution' },
-            {
-                icon: 'sports_football',
-                label: 'Go to Sleeper',
-                dest: `https://sleeper.app/leagues/${leagueID}`,
-            },
+            { icon: 'sports_football', label: 'Go to Sleeper', dest: `https://sleeper.app/leagues/${leagueID}` },
         ]
     },
     {
-        icon: 'build',
-        label: 'League Tools',
-        nest: true,
-        key: 'league_tools',
+        icon: 'build', label: 'League Tools', nest: true, key: 'league_tools',
         children: [
-            {
-                icon: 'school',
-                label: 'Future Prospects',
-                dest: '/future-prospects',
-            },
+            { icon: 'school', label: 'Future Prospects', dest: '/future-prospects' },
+            { icon: 'insights', label: 'Owner Power Rating', dest: '/power-rating' },
         ]
     },
-    {
-        icon: 'lightbulb',
-        label: 'Resources',
-        dest: '/resources',
-        key: 'resources',
-    },
+    { icon: 'lightbulb', label: 'Resources', dest: '/resources', key: 'resources' },
 ];
